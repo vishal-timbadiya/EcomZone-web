8231c704b658cc5eef12ff28d699a4d04defbfbd
Pratik <pratikkachhadiya1999@gmail.com>
2026-06-09 16:37:51 +0530

    fix: update Prisma client configuration

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+  prisma: PrismaClient | undefined;
 };
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["query"],
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
   });
 
-if (process.env.NODE_ENV !== "production")
+if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
+}
